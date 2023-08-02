edd9c44d6464e6972210e3d2d61d00fd1d889d33
Aaron Smith <smithaaron654@gmail.com>
2023-08-01 21:17:42 -0400

    added stat button to display current player stats

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 //linking elements from external js file to html 
 const textPrompt = document.getElementById("textPrompt");
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('#choiceContainer button');
+const statButton = document.getElementById("statButton");
+const statList = document.getElementById("statList")
 
 const player = {
     //name: prompt("What is your name?"),
-    name: "Aaron",
+    name: "Player",
     HP: 100,
     MP: 5,
     Strength: 5,
     Speed: 5,
     Intelligence: 5
-}
+};
+
+statButton.onclick = function(){
+    if (statList.style.display = "none"){
+        statList.style.display = "none"
+    } else {
+        statList.style.display = "flex"
+    }
+};
 
 //textPrompt.innerHTML = `${player.name}, it is nice to meet you! Get ready for an adventure!`
 
-function controlButtons(){
+function controlButtons() {
     buttons.forEach(function (e) {
-        e.addEventListener("click", function() {
-          console.log(e.innerHTML);
-          textPrompt.innerHTML = (`You have chosen: ${e.innerHTML}`)
+        e.addEventListener("click", function () {
+            console.log(e.innerHTML);
+            textPrompt.innerHTML = (`You have chosen: ${e.innerHTML}`)
         });
-      });
-}
+    });
+};
 
 controlButtons();
 
